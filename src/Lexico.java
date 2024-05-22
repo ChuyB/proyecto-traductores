3913375f4c807eb8b6b4f5d8e29c5b19a1910b01
vblanyer <vblanyer@gmail.com>
2024-05-21 21:47:34 -0400

    lexico

--- src/Lexico.java
+++ src/Lexico.java
@@ -1,5 +1,21 @@
 package src;
 
-public class Lexico {
+import java.util.ArrayList;
+import src.TokenKind;
+import src.Token;
 
+public class Lexico {
+      private ArrayList<Token> tokens;
+      private int fila;
+      private int columna;
+
+      public Lexico() {
+            this.tokens = new ArrayList<Token>();
+            this.fila = 0;
+            this.columna = 0;
+      }
+
+      public void analizar(String line) {
+
+      }
 }
