d07f8744aff4fa85444a8e3e4fd3538d76d32644
ChuyB <bovea3359@gmail.com>
2024-05-23 18:19:42 -0400

    feat/añade el manejador para la lectura de archivo

--- lexer/src/main/java/org/example/lib/ManejadorArchivo.java
+++ lexer/src/main/java/org/example/lib/ManejadorArchivo.java
@@ -4,18 +4,32 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
+/**
+ * Clase que maneja la lectura de un archivo.
+ */
 public class ManejadorArchivo {
   private String dirArchivo;
   private StringBuilder contenido = new StringBuilder();
 
+  /**
+   * Constructor de la clase.
+   * @param dirArchivo Dirección del archivo a leer.
+   */
   public ManejadorArchivo(String dirArchivo) {
     this.dirArchivo = dirArchivo;
   }
 
+  /**
+   * Obtiene el contenido del archivo.
+   * @return Contenido del archivo.
+   */
   public String getContenido() {
     return contenido.toString();
   }
 
+  /**
+   * Procesa el archivo y guarda su contenido.
+   */
   public void procesarArchivo() {
     try (BufferedReader lector = new BufferedReader(new FileReader(dirArchivo))) {
       String linea;
